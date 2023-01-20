1217f00d385823b4e617e49d647626524b0bbdd6
Yihen <liuyihen@gmail.com>
2023-01-20 10:27:55 +0800

    local branch setup

--- www/js/monitor/monitor.js
+++ www/js/monitor/monitor.js
@@ -6,7 +6,7 @@
 
 //const gApiHost = "https://" + window.location.hostname
 // == "mempool.observer" ? "https://mempool.observer" : "https://dev77.mempool.observer"
-const gApiHost = "https://www.openchart.org:4444"
+const gApiHost = "http://127.0.0.1:23485"
 
 async function loadEntryData(){
   if(gData == null){
